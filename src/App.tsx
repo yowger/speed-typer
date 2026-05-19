edb458e7a1c6744ef6cadcad9a616e2c6a6aff53
Ver0018 <rogerpantilyowger@gmail.com>
2026-05-20 02:06:09 +0800

    added spacing

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { cn } from "./utils/cn"
 const text =
     "Biology explores the living world, covering organisms' structure, function, and evolution, with genetics and ecology being key areas. Modern tools like CRISPR allow for gene manipulation, while ecology examines environmental interactions. Biology's applications include medicine, agriculture, and conservation, making it essential for understanding life and addressing global challenges."
 
+const words = text.split(" ")
+
 type CharState = {
     char: string
     status: "correct" | "incorrect"
@@ -13,12 +15,12 @@ type CharState = {
 export default function App() {
     const [currentIndex, setCurrentIndex] = useState(0)
     const [typed, setTyped] = useState<CharState[]>([])
-    // const [startedAt, setStartedAt] = useState<number | null>(null)
     const [mistakes, setMistakes] = useState(0)
-    console.log("mistakes: ", mistakes)
 
     useEffect(() => {
         const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.key.length > 1 && event.key !== " ") return
+
             const expectedChar = text[currentIndex]
 
             const isCorrect = event.key === expectedChar
@@ -35,12 +37,7 @@ export default function App() {
                 setMistakes((prev) => prev + 1)
             }
 
-            if (event.key === text[currentIndex]) {
-                setCurrentIndex((prevIndex) => prevIndex + 1)
-            }
-
-            console.log("Key pressed:", event.key, "Expected:", expectedChar)
-            console.log("typed: ", typed)
+            setCurrentIndex((prev) => prev + 1)
         }
 
         window.addEventListener("keydown", handleKeyDown)
@@ -48,37 +45,96 @@ export default function App() {
         return () => {
             window.removeEventListener("keydown", handleKeyDown)
         }
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [currentIndex])
 
+    let globalIndex = 0
+
     return (
-        <div className="flex h-screen items-center justify-center">
-            <div className="text-2xl font-mono p-8">
-                {text.split("").map((char, index) => {
-                    const typedChar = typed[index]
+        <div className="flex min-h-screen items-center justify-center bg-black text-white">
+            <div className="mt-20 max-w-4xl">
+                <div className="flex flex-wrap gap-y-4 p-8 text-2xl font-mono leading-relaxed">
+                    {words.map((word, wordIndex) => {
+                        return (
+                            <div key={wordIndex} className="flex">
+                                {word.split("").map((char, charIndex) => {
+                                    const typedChar = typed[globalIndex]
 
-                    return (
-                        <span
-                            className={cn(
-                                index === currentIndex && "border-b",
+                                    const currentCharIndex = globalIndex
 
-                                typedChar?.status === "correct" &&
-                                    "text-green-500",
+                                    globalIndex++
 
-                                typedChar?.status === "incorrect" &&
-                                    "text-red-500",
+                                    return (
+                                        <span
+                                            key={charIndex}
+                                            className={cn(
+                                                "transition-colors border-b-2 border-transparent",
 
-                                !typedChar &&
-                                    index !== currentIndex &&
-                                    "text-gray-500",
-                            )}
-                            key={index}
-                        >
-                            {char}
-                        </span>
-                    )
-                })}
+                                                currentCharIndex ===
+                                                    currentIndex &&
+                                                    "border-b-4 border-white",
+
+                                                typedChar?.status ===
+                                                    "correct" &&
+                                                    "text-green-500",
+
+                                                typedChar?.status ===
+                                                    "incorrect" &&
+                                                    "text-red-500",
+
+                                                !typedChar &&
+                                                    currentCharIndex !==
+                                                        currentIndex &&
+                                                    "text-gray-500",
+                                            )}
+                                        >
+                                            {char}
+                                        </span>
+                                    )
+                                })}
+
+                                {wordIndex !== words.length - 1 &&
+                                    (() => {
+                                        const typedChar = typed[globalIndex]
+
+                                        const currentCharIndex = globalIndex
+
+                                        globalIndex++
+
+                                        return (
+                                            <span
+                                                className={cn(
+                                                    "w-4 transition-colors",
+
+                                                    currentCharIndex ===
+                                                        currentIndex &&
+                                                        "border-b-2 border-white",
+
+                                                    typedChar?.status ===
+                                                        "correct" &&
+                                                        "text-green-500",
+
+                                                    typedChar?.status ===
+                                                        "incorrect" &&
+                                                        "bg-red-500/30",
+
+                                                    !typedChar &&
+                                                        currentCharIndex !==
+                                                            currentIndex &&
+                                                        "text-gray-500",
+                                                )}
+                                            >
+                                                {" "}
+                                            </span>
+                                        )
+                                    })()}
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <div className="px-8 text-sm text-gray-400">
+                    Mistakes: {mistakes}
+                </div>
             </div>
         </div>
     )
